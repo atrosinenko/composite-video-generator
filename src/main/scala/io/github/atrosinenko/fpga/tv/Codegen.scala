98323e636d76ddc4304761e5a0604c2248d5f200
Anatoly Trosinenko <anatoly.trosinenko@gmail.com>
2018-07-02 16:39:01 +0300

    Attach oscilloscope to the project
    It is attached in such a way that it records either real signal
    from ADC or its generated video signal in its original
    digital form depending on "least significant LED".

--- src/main/scala/io/github/atrosinenko/fpga/tv/Codegen.scala
+++ src/main/scala/io/github/atrosinenko/fpga/tv/Codegen.scala
@@ -6,13 +6,23 @@ import io.github.atrosinenko.fpga.common.PWM
 object Codegen {
   class TestModule(mhz: Int) extends Module {
     val io = IO(new Bundle {
+      val signal = Input(UInt(8.W))
+      val button1 = Input(Bool())
+      val button2 = Input(Bool())
+      val leds = Output(UInt(4.W))
+
       val millivolts = Output(UInt(12.W))
     })
-    val imageGenerator = Module(new TestColorImageGenerator(540, 400))
+    val imageGenerator = Module(new TestColorImageGenerator(mhz, 540, 400))
     val encoder = Module(new PalGenerator(clocksPerUs = mhz))
     imageGenerator.io.x <> encoder.io.x
     imageGenerator.io.y <> encoder.io.y
 
+    imageGenerator.io.signal1  <> io.signal
+    imageGenerator.io.signal2  <> io.millivolts(9, 2)
+    imageGenerator.io.button1 <> io.button1
+    imageGenerator.io.button2 <> io.button2
+    imageGenerator.io.leds <> io.leds
     imageGenerator.io.red   <> encoder.io.red
     imageGenerator.io.green <> encoder.io.green
     imageGenerator.io.blue  <> encoder.io.blue
--- src/main/scala/io/github/atrosinenko/fpga/tv/TestColorImageGenerator.scala
+++ src/main/scala/io/github/atrosinenko/fpga/tv/TestColorImageGenerator.scala
@@ -1,9 +1,18 @@
 package io.github.atrosinenko.fpga.tv
 
 import chisel3._
+import io.github.atrosinenko.fpga.common.SimpleButtonController
+import io.github.atrosinenko.fpga.oscilloscope.{Oscilloscope, OscilloscopeController}
 
-class TestColorImageGenerator(width: Int, height: Int) extends Module {
+class TestColorImageGenerator(mhz: Int, width: Int, height: Int) extends Module {
   val io = IO(new Bundle {
+    val signal1 = Input(UInt(8.W))
+    val signal2 = Input(UInt(8.W))
+
+    val button1 = Input(Bool())
+    val button2 = Input(Bool())
+    val leds = Output(UInt(4.W))
+
     val red   = Output(UInt(8.W))
     val green = Output(UInt(8.W))
     val blue  = Output(UInt(8.W))
@@ -12,7 +21,25 @@ class TestColorImageGenerator(width: Int, height: Int) extends Module {
     val y = Input(UInt(10.W))
   })
 
-  io.red   := Mux((io.x / 32.asUInt + io.y / 32.asUInt)(0), 200.asUInt, 0.asUInt)
-  io.green := Mux((io.x / 32.asUInt + io.y / 32.asUInt)(0), 200.asUInt, 0.asUInt)
-  io.blue  := Mux((io.x / 32.asUInt + io.y / 32.asUInt)(0), 0.asUInt, 0.asUInt)
+  private val oscilloscopeController = Module(
+    new OscilloscopeController(
+      512,
+      () => new SimpleButtonController((0.1 * mhz * 1e6).toInt, (0.2 * mhz * 1e6).toInt, (0.3 * mhz * 1.23e6).toInt, false)
+    )
+  )
+  private val oscilloscope = Module ( new Oscilloscope(clocksPerUs = mhz, inputWidth = 8, windowPixelWidth = 512, windowPixelHeight = 400))
+
+  oscilloscopeController.io.button1 <> io.button1
+  oscilloscopeController.io.button2 <> io.button2
+  oscilloscopeController.io.leds <> io.leds
+  oscilloscope.io.signal := Mux(io.leds(0), io.signal1, io.signal2)
+  oscilloscope.io.start  <> oscilloscopeController.io.start
+  oscilloscope.io.visualOffset <> oscilloscopeController.io.visibleOffset
+  oscilloscope.io.x <> io.x
+  oscilloscope.io.y <> io.y
+
+  private val t = oscilloscope.io.output
+  io.red   := Mux(t, 0.asUInt, 0.asUInt)
+  io.green := Mux(t, 0.asUInt, 200.asUInt)
+  io.blue  := Mux(t, 0.asUInt, 0.asUInt)
 }
